ebfeb741de3fe864a5a711a5c3c79ff4da599168
DennisN22042003 <dennisnjuguna278@gmail.com>
2025-04-26 16:04:52 +0300

    Initial Commit

---
+++ src/main/java/com/example/SinnersGame/SinnersGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.SinnersGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinnersGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinnersGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SinnersGame/SinnersGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SinnersGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinnersGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
